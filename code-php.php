84611de52b26b3a8191cda58cbb9cb5df7ccf29f
Lina <144618594+rinrina01@users.noreply.github.com>
2023-12-01 11:32:41 +0100

    Update code-php.php

--- code-php.php
+++ code-php.php
@@ -2,11 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-define('ADMIN_MAIL', 'mail@gmail.com');
-define('PROJECT_FOLDER', '/MEMORY/');
-define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . PROJECT_FOLDER);
-
 function connectToDbAndGetPdo(): PDO {
     $dbname = 'MEMORY';
     $host = 'localhost';
